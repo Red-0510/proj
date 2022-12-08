eeb1a49473a39f6836afa98ae297eae0a87ae2a5
Red-0510 <jaineshmachhi@gmail.com>
2022-12-08 11:39:16 +0530

    database to blog webiste

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 mongoose.set("strictQuery",true);
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{
+mongoose.connect("mongodb+srv://admin:admin@cluster0.itedlkj.mongodb.net/todolistDB",{
   useNewUrlParser:true,
 });
 
